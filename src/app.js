265355deb1afebca9e09be1b39cca7d215903a47
Aditia <aditpraz222@gmail.com>
2024-11-28 18:04:25 +0700

    feat: add Vercel config and build script for deployment

--- src/app.js
+++ src/app.js
@@ -1,40 +1,24 @@
 const express = require("express");
-
 const app = express();
-
 const dotenv = require("dotenv");
-
 dotenv.config();
-
 const adminAuthorization = require("./middleware/adminAuthorization");
-
 const cors = require("cors");
-
-const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(cors());
-
 app.get("/", (req, res) => {  
   res.send("Hello there!");
 }); 
 
 const authController = require("./auth/auth.controller");
-
 const itemController = require("./item/item.controller");
-
 const userController = require("./user/user.controller");
-
 const transactionController = require("./transaction/transaction.controller");
 
 app.use("/api/transactions", transactionController);
-
 app.use("/api/auth", authController);
-
 app.use("/api/items", itemController);
-
 app.use("/api/users", adminAuthorization, userController);
 
-app.listen(PORT, () => {
-  console.log(`App listening on port ` + PORT);
-});
+export default app:
